4205a504766dba4225f958878b982d91cf6962c0
Jordan Paris <hamologist@gmail.com>
2018-12-25 22:40:00 -0500

    Detach env var processing logic into a separate py project

--- flask_auth0/__init__.py
+++ flask_auth0/__init__.py
@@ -1,7 +1,7 @@
 from typing import List
 
 from .utility import handle_auth_error, get_token_auth_header, requires_auth, requires_scope
-from .environment import process_environment_variable, SupportedBaseTypes
+from env_process import process_environment_variable, SupportedBaseTypes
 
 
 AUTH0_DOMAIN: str = process_environment_variable('AUTH0_DOMAIN', env_type=SupportedBaseTypes.STR)
--- setup.py
+++ setup.py
@@ -4,5 +4,12 @@ setup(
     name='flask-auth0',
     version='0.1',
     license='MIT',
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires=[
+        'flask>=1.0',
+        'auth0-python>=3.4',
+        'python-dotenv>=0.10',
+        'requests>=2.20',
+        'python-jose>=3.0',
+    ]
 )
